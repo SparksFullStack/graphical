d3aeabfd1f3c8a1f83b626a874ab980721a45194
SparksFullStack <csparks@antioch.edu>
2019-04-01 17:12:13 -0700

    initial graphql build up and running on route :3001/graphql

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 
+// Importing our Schema
+const schema = require('./schema/schema');
+
 const port = process.env.PORT || 3001;
 const server = express();
 
 server.use(express.json());
+// Making use of middleware to enable Graphiql based requests
 server.use('/graphql', expressGraphQL({
+    schema: schema,
     graphiql: true
 }));
 
